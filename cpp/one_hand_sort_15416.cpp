8d96fc38a05b665ba340357747c9d9a863c416ef
DhruvSharma845 <dhruvsharma845@gmail.com>
2021-01-16 21:56:13 +0530

    Solutions: OneHandSort and OppositeParity

---
+++ cpp/one_hand_sort_15416.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class OneHandSort {
+public:
+    std::vector<int> sortShelf(std::vector<int> target);
+};
+
+std::vector<int> OneHandSort::sortShelf(std::vector<int> target) {
+    
+    std::vector<int> steps;
+    int extraPos = -1;
+    for(int currentIndex = 0; currentIndex < target.size(); ++currentIndex) {
+        int i = currentIndex;
+        if(i != target[i]) {
+            extraPos = target[i];
+            steps.push_back(i);
+            while(i != extraPos && i != target[i]) {
+                int j = 0;
+                for(; j < target.size(); ++j) {
+                    if(target[j] == i) {
+                        break;
+                    }
+                }
+                target[i] = i;
+                i = j;
+                steps.push_back(j);
+            }
+            if(i == extraPos) {
+                target[i] = i;
+                steps.push_back(target.size());
+            }
+        }
+    }
+    return steps;
+}
+
+int main() {
+    OneHandSort ohs;
+    std::vector<int> arr{1, 2, 3, 0};
+    std::vector<int> sortedArr = ohs.sortShelf(arr);
+
+    std::copy(sortedArr.begin(), sortedArr.end(), std::ostream_iterator<int>(std::cout, " "));
+}
---
+++ cpp/opposite_parity_15945.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+class OppositeParity {
+public:
+    std::vector<int> rearrange(std::vector<int>& A);
+};
+
+std::vector<int> OppositeParity::rearrange(std::vector<int>& A) {
+    int MAX_VAL = 1000;
+
+    int oddIndex = 0;
+    int evenIndex = 0;
+
+    while(true) {
+        while(oddIndex < A.size() && (A[oddIndex] > MAX_VAL || A[oddIndex] % 2 == 0)) {
+            ++oddIndex;
+        }
+
+        while(evenIndex < A.size() && (A[evenIndex] > MAX_VAL || A[evenIndex] % 2 != 0)) {
+            ++evenIndex;
+        }
+
+        if(oddIndex >= A.size() && evenIndex >= A.size()) {
+            break;
+        }
+
+        if(oddIndex >= A.size() || evenIndex >= A.size()) {
+            return std::vector<int>();
+        }
+
+        int evenValue = A[evenIndex];
+        int oddValue = A[oddIndex];
+
+        A[evenIndex] = MAX_VAL + oddValue;
+        A[oddIndex] = MAX_VAL + evenValue; 
+
+        ++oddIndex;
+        ++evenIndex;
+    }
+
+    for(int i = 0; i < A.size(); ++i) {
+        A[i] -= MAX_VAL;
+    }
+
+    return A;
+}
+
+/**
+ *  1 4 1 4 2 1
+ *  
+ **/
+int main() {
+    OppositeParity op;
+    std::vector<int> v{27, 18, 281, 828, 45, 90, 452, 3};
+    std::vector<int> res = op.rearrange(v);
+    std::copy(res.begin(), res.end(), std::ostream_iterator<int>(std::cout, " "));
+}
